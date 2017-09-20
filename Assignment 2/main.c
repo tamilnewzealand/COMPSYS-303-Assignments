f65fdbb3b12bdee24ef7de5ac8f4e041cf9675b7
சகாயன் சிற்சபேசன் <ssit662@aucklanduni.ac.nz>
2017-09-20 21:00:43 +1200

    fixed non-blocking UART

--- Assignment 2/main.c
+++ Assignment 2/main.c
@@ -5,171 +5,158 @@
  *      Author: சகாயன் சிற்சபேசன்
  */
 
-#include "defines.h"
-#include "sccharts.h"
-
-alt_u32 avi_timer_isr(void* context)
-{
-	AVITO = 1;
-	return 0;
-}
-
-alt_u32 aei_timer_isr(void* context)
-{
-	AEITO = 1;
-	return 0;
-}
-
-alt_u32 pvarp_timer_isr(void* context)
-{
-	PVARPTO = 1;
-	return 0;
-}
-
-alt_u32 vrp_timer_isr(void* context)
-{
-	VRPTO = 1;
-	return 0;
-}
-
-alt_u32 lri_timer_isr(void* context)
-{
-	LRITO = 1;
-	return 0;
-}
-
-alt_u32 uri_timer_isr(void* context)
-{
-	URITO = 1;
-	return 0;
-}
-
-void init_buttons_pio()
-{
-	void* context = NULL;
-
-	// clears the edge capture register
-	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(BUTTONS_BASE, 0);
-	// enable interrupts for all buttons
-	IOWR_ALTERA_AVALON_PIO_IRQ_MASK(BUTTONS_BASE, 0x7);
-
-	// register the ISR
-	alt_irq_register(BUTTONS_IRQ, context, buttons_isr);
-}
-
-void buttons_isr(void* context, alt_u32 id)
-{
-	int* temp = (int*) context; // need to cast the context first before using it
-	(*temp) = IORD_ALTERA_AVALON_PIO_EDGE_CAP(BUTTONS_BASE);
-	// clear the edge capture register
-	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(BUTTONS_BASE, 0);
-
-	if (((*temp) & 0x02) > 0)
-	{
-		if (oldKEY1 == 0) ASense = 1;
-		oldKEY1 = 1;
-	}
-	else oldKEY1 = 0;
-
-	if (((*temp) & 0x01) > 0)
-	{
-		if (oldKEY2 == 0) VSense = 1;
-		oldKEY2 = 1;
-	}
-	else oldKEY2 = 0;
-}
-
-void init_uart()
-{
-	void* context = NULL;
-
-	// register the ISR
-	alt_irq_register(UART_IRQ, context, uart_RecvBufferIsr);
-}
-
-void uart_RecvBufferIsr(void *context, alt_u32 id)
-{
-	recv_char = IORD_ALTERA_AVALON_UART_RXDATA(UART_BASE);
-	if (recv_char == 'A') {
-		ASense = 1;
-		printf("A \n");
-	}
-	if (recv_char == 'V') {
-		VSense = 1;
-		printf("V \n");
-	}
-}
-
-int main()
-{
-	init_buttons_pio();
-	init_uart();
-	reset();
-
-	while(1)
-	{
-		tick();
-
-		// Starts the timers if event occurred
-		if (AVI_start == 1) alt_alarm_start(&timer_AVI, AVI_VALUE, avi_timer_isr, NULL);
-		if (AEI_start == 1) alt_alarm_start(&timer_AEI, AEI_VALUE, aei_timer_isr, NULL);
-		if (PVARP_start == 1) alt_alarm_start(&timer_PVARP, PVARP_VALUE, pvarp_timer_isr, NULL);
-		if (VRP_start == 1) alt_alarm_start(&timer_VRP, VRP_VALUE, vrp_timer_isr, NULL);
-		if (LRI_start == 1) alt_alarm_start(&timer_LRI, LRI_VALUE, lri_timer_isr, NULL);
-		if (URI_start == 1) alt_alarm_start(&timer_URI, URI_VALUE, uri_timer_isr, NULL);
-		
-		// Stops the timers if event occurred
-		if (AVI_stop == 1) alt_alarm_stop(&timer_AVI);
-		if (AEI_stop == 1) alt_alarm_stop(&timer_AEI);
-		if (PVARP_stop == 1) alt_alarm_stop(&timer_PVARP);
-		if (VRP_stop == 1) alt_alarm_stop(&timer_VRP);
-		if (LRI_stop == 1) alt_alarm_stop(&timer_LRI);
-		if (URI_stop == 1) alt_alarm_stop(&timer_URI);
-
-		// reseting inputs
-		VSense = 0;
-		ASense = 0;
-
-		// resetting outputs
-		AVITO = 0;
-		PVARPTO = 0;
-		VRPTO = 0;
-		AEITO = 0;
-		LRITO = 0;
-		URITO = 0;
-
-		// Buffering outputs
-		if (APace == 1)
-		{
-			LEDAPace = APace;
-			LEDBufferCountA = LED_BUFFER;
-		}
-
-		if (VPace == 1)
-		{
-			LEDVPace = VPace;
-			LEDBufferCountV = LED_BUFFER;
-		}
-
-		LEDBufferCountA--;
-		LEDBufferCountV--;
-
-		if (LEDBufferCountA == 0) LEDAPace = 0;
-		if (LEDBufferCountV == 0) LEDVPace = 0;
-
-		// Outputting buffered values to LEDs
-		if (LEDVPace == 1)
-		{
-			if (LEDAPace == 1) IOWR_ALTERA_AVALON_PIO_DATA(LEDS_GREEN_BASE, 0x03);
-			else IOWR_ALTERA_AVALON_PIO_DATA(LEDS_GREEN_BASE, 0x01);
-		}
-		else
-		{
-			if (LEDAPace == 1) IOWR_ALTERA_AVALON_PIO_DATA(LEDS_GREEN_BASE, 0x02);
-			else IOWR_ALTERA_AVALON_PIO_DATA(LEDS_GREEN_BASE, 0x00);
-		}
-
-	}
-	return 0;
-}
-
+ #include "defines.h"
+ #include "sccharts.h"
+ 
+ alt_u32 avi_timer_isr(void* context)
+ {
+	 AVITO = 1;
+	 return 0;
+ }
+ 
+ alt_u32 aei_timer_isr(void* context)
+ {
+	 AEITO = 1;
+	 return 0;
+ }
+ 
+ alt_u32 pvarp_timer_isr(void* context)
+ {
+	 PVARPTO = 1;
+	 return 0;
+ }
+ 
+ alt_u32 vrp_timer_isr(void* context)
+ {
+	 VRPTO = 1;
+	 return 0;
+ }
+ 
+ alt_u32 lri_timer_isr(void* context)
+ {
+	 LRITO = 1;
+	 return 0;
+ }
+ 
+ alt_u32 uri_timer_isr(void* context)
+ {
+	 URITO = 1;
+	 return 0;
+ }
+ 
+ void init_buttons_pio()
+ {
+	 void* context = NULL;
+ 
+	 // clears the edge capture register
+	 IOWR_ALTERA_AVALON_PIO_EDGE_CAP(BUTTONS_BASE, 0);
+	 // enable interrupts for all buttons
+	 IOWR_ALTERA_AVALON_PIO_IRQ_MASK(BUTTONS_BASE, 0x7);
+ 
+	 // register the ISR
+	 alt_irq_register(BUTTONS_IRQ, context, buttons_isr);
+ }
+ 
+ void buttons_isr(void* context, alt_u32 id)
+ {
+	 int* temp = (int*) context; // need to cast the context first before using it
+	 (*temp) = IORD_ALTERA_AVALON_PIO_EDGE_CAP(BUTTONS_BASE);
+	 // clear the edge capture register
+	 IOWR_ALTERA_AVALON_PIO_EDGE_CAP(BUTTONS_BASE, 0);
+ 
+	 if (((*temp) & 0x02) > 0)
+	 {
+		 if (oldKEY1 == 0) ASense = 1;
+		 oldKEY1 = 1;
+	 }
+	 else oldKEY1 = 0;
+ 
+	 if (((*temp) & 0x01) > 0)
+	 {
+		 if (oldKEY2 == 0) VSense = 1;
+		 oldKEY2 = 1;
+	 }
+	 else oldKEY2 = 0;
+ }
+ 
+ int main()
+ {
+	 init_buttons_pio();
+	 up = open(UART_NAME, O_RDWR | O_NONBLOCK);
+	 reset();
+ 
+	 while(1)
+	 {
+		 if ((nbr = read(up, buffer, 1)) > 0) {
+			 if (buffer[0] == 65) ASense = 1; // A
+			 if (buffer[0] == 86) VSense = 1; // V
+		 }
+ 
+		 tick();
+ 
+		 // Starts the timers if event occurred
+		 if (AVI_start == 1) alt_alarm_start(&timer_AVI, AVI_VALUE, avi_timer_isr, NULL);
+		 if (AEI_start == 1) alt_alarm_start(&timer_AEI, AEI_VALUE, aei_timer_isr, NULL);
+		 if (PVARP_start == 1) alt_alarm_start(&timer_PVARP, PVARP_VALUE, pvarp_timer_isr, NULL);
+		 if (VRP_start == 1) alt_alarm_start(&timer_VRP, VRP_VALUE, vrp_timer_isr, NULL);
+		 if (LRI_start == 1) alt_alarm_start(&timer_LRI, LRI_VALUE, lri_timer_isr, NULL);
+		 if (URI_start == 1) alt_alarm_start(&timer_URI, URI_VALUE, uri_timer_isr, NULL);
+ 
+		 // Stops the timers if event occurred
+		 if (AVI_stop == 1) alt_alarm_stop(&timer_AVI);
+		 if (AEI_stop == 1) alt_alarm_stop(&timer_AEI);
+		 if (PVARP_stop == 1) alt_alarm_stop(&timer_PVARP);
+		 if (VRP_stop == 1) alt_alarm_stop(&timer_VRP);
+		 if (LRI_stop == 1) alt_alarm_stop(&timer_LRI);
+		 if (URI_stop == 1) alt_alarm_stop(&timer_URI);
+ 
+		 // reseting inputs
+		 VSense = 0;
+		 ASense = 0;
+ 
+		 // resetting outputs
+		 AVITO = 0;
+		 PVARPTO = 0;
+		 VRPTO = 0;
+		 AEITO = 0;
+		 LRITO = 0;
+		 URITO = 0;
+ 
+		 // Buffering outputs
+		 if (APace == 1)
+		 {
+			 LEDAPace = APace;
+			 LEDBufferCountA = LED_BUFFER;
+		 }
+ 
+		 if (VPace == 1)
+		 {
+			 LEDVPace = VPace;
+			 LEDBufferCountV = LED_BUFFER;
+		 }
+ 
+		 LEDBufferCountA--;
+		 LEDBufferCountV--;
+ 
+		 if (LEDBufferCountA == 0) LEDAPace = 0;
+		 if (LEDBufferCountV == 0) LEDVPace = 0;
+ 
+		 // Outputting buffered values to LEDs
+		 if (LEDVPace == 1)
+		 {
+			 if (LEDAPace == 1) IOWR_ALTERA_AVALON_PIO_DATA(LEDS_GREEN_BASE, 0x03);
+			 else IOWR_ALTERA_AVALON_PIO_DATA(LEDS_GREEN_BASE, 0x01);
+		 }
+		 else
+		 {
+			 if (LEDAPace == 1) IOWR_ALTERA_AVALON_PIO_DATA(LEDS_GREEN_BASE, 0x02);
+			 else IOWR_ALTERA_AVALON_PIO_DATA(LEDS_GREEN_BASE, 0x00);
+		 }
+ 
+	 }
+ 
+	 close(up);
+	 return 0;
+ }
+ 
+ 
