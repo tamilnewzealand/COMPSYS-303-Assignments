8fe4819040a5867501c6d09469839ee09b42ea72
சகாயன் சிற்சபேசன் <ssit662@aucklanduni.ac.nz>
2017-09-21 23:17:20 +1200

    misc changes

--- Assignment 2/defines.h
+++ Assignment 2/defines.h
@@ -5,53 +5,63 @@
  *      Author: சகாயன் சிற்சபேசன்
  */
 
-#ifndef DEFINES_H_
-#define DEFINES_H_
-
-#include <stdio.h>
-#include <system.h>
-#include <sys/alt_alarm.h>
-#include <altera_avalon_pio_regs.h>
-#include <alt_types.h>
-#include <sys/alt_irq.h>
-
-// Example timeout values in milliseconds
-#define AVI_VALUE 300
-#define AEI_VALUE 800
-#define PVARP_VALUE 50
-#define VRP_VALUE 150
-#define LRI_VALUE 950
-#define URI_VALUE 900
-#define LED_BUFFER 500
-
-static volatile unsigned int oldKEY1;
-static volatile unsigned int oldKEY2;
-
-static volatile int recv_char;
-
-static unsigned int LEDVPace;
-static unsigned int LEDAPace;
-static unsigned int LEDBufferCountA;
-static unsigned int LEDBufferCountV;
-
-alt_u32 avi_timer_isr(void* context);
-alt_u32 aei_timer_isr(void* context);
-alt_u32 pvarp_timer_isr(void* context);
-alt_u32 vrp_timer_isr(void* context);
-alt_u32 lri_timer_isr(void* context);
-alt_u32 uri_timer_isr(void* context);
-
-static alt_alarm timer_AVI;
-static alt_alarm timer_AEI;
-static alt_alarm timer_PVARP;
-static alt_alarm timer_VRP;
-static alt_alarm timer_LRI;
-static alt_alarm timer_URI;
-
-void init_buttons_pio();
-void buttons_isr(void* context, alt_u32 id);
-void init_uart();
-void uart_RecvBufferIsr(void *context, alt_u32 id);
-int main();
-
-#endif /* DEFINES_H_ */
+ #ifndef DEFINES_H_
+ #define DEFINES_H_
+ 
+ #include <stdio.h>
+ #include <system.h>
+ #include <sys/alt_alarm.h>
+ #include <altera_avalon_pio_regs.h>
+ #include <alt_types.h>
+ #include <sys/alt_irq.h>
+ #include <fcntl.h>
+ #include <unistd.h>
+ #include <string.h>
+ #include <ctype.h>
+ 
+ // Example timeout values in milliseconds
+ #define AVI_VALUE 300
+ #define AEI_VALUE 800
+ #define PVARP_VALUE 50
+ #define VRP_VALUE 150
+ #define LRI_VALUE 950
+ #define URI_VALUE 900
+ #define LED_BUFFER 500
+ 
+ static volatile unsigned int oldKEY1;
+ static volatile unsigned int oldKEY2;
+ 
+ static volatile int recv_char;
+ 
+ static unsigned int LEDVPace;
+ static unsigned int LEDAPace;
+ static unsigned int LEDBufferCountA;
+ static unsigned int LEDBufferCountV;
+ 
+ FILE* up;
+ FILE* fp;
+ char buffer[80];
+ int nbr;
+ 
+ alt_u32 avi_timer_isr(void* context);
+ alt_u32 aei_timer_isr(void* context);
+ alt_u32 pvarp_timer_isr(void* context);
+ alt_u32 vrp_timer_isr(void* context);
+ alt_u32 lri_timer_isr(void* context);
+ alt_u32 uri_timer_isr(void* context);
+ 
+ static alt_alarm timer_AVI;
+ static alt_alarm timer_AEI;
+ static alt_alarm timer_PVARP;
+ static alt_alarm timer_VRP;
+ static alt_alarm timer_LRI;
+ static alt_alarm timer_URI;
+ 
+ void init_buttons_pio();
+ void buttons_isr(void* context, alt_u32 id);
+ void init_uart();
+ void uart_RecvBufferIsr(void *context, alt_u32 id);
+ int main();
+ 
+ #endif /* DEFINES_H_ */
+ 
--- Assignment 2/main.c
+++ Assignment 2/main.c
@@ -35,6 +35,7 @@
  alt_u32 lri_timer_isr(void* context)
  {
 	 LRITO = 1;
+	 printf("\nLRI Timed out");
 	 return 0;
  }
  
@@ -79,83 +80,105 @@
 	 else oldKEY2 = 0;
  }
  
+ void read_uart()
+ {
+	 if ((nbr = read(up, buffer, 1)) > 0) {
+		 if (buffer[0] == 65) ASense = 1; // A
+		 if (buffer[0] == 86) VSense = 1; // V
+	 }
+ }
+ 
+ void start_stop_timers()
+ {
+	 // Starts the timers if event occurred
+	 if (AVI_start == 1) alt_alarm_start(&timer_AVI, AVI_VALUE, avi_timer_isr, NULL);
+	 if (AEI_start == 1) alt_alarm_start(&timer_AEI, AEI_VALUE, aei_timer_isr, NULL);
+	 if (PVARP_start == 1) alt_alarm_start(&timer_PVARP, PVARP_VALUE, pvarp_timer_isr, NULL);
+	 if (VRP_start == 1) alt_alarm_start(&timer_VRP, VRP_VALUE, vrp_timer_isr, NULL);
+	 if (LRI_start == 1) alt_alarm_start(&timer_LRI, LRI_VALUE, lri_timer_isr, NULL);
+	 if (URI_start == 1) alt_alarm_start(&timer_URI, URI_VALUE, uri_timer_isr, NULL);
+ 
+	 // Stops the timers if event occurred
+	 if (AVI_stop == 1) alt_alarm_stop(&timer_AVI);
+	 if (AEI_stop == 1) alt_alarm_stop(&timer_AEI);
+	 if (PVARP_stop == 1) alt_alarm_stop(&timer_PVARP);
+	 if (VRP_stop == 1) alt_alarm_stop(&timer_VRP);
+	 if (LRI_stop == 1) alt_alarm_stop(&timer_LRI);
+	 if (URI_stop == 1) alt_alarm_stop(&timer_URI);
+ }
+ 
+ void reset_inputs()
+ {
+	 // reseting inputs
+	 VSense = 0;
+	 ASense = 0;
+ 
+	 // resetting outputs
+	 AVITO = 0;
+	 PVARPTO = 0;
+	 VRPTO = 0;
+	 AEITO = 0;
+	 LRITO = 0;
+	 URITO = 0;
+ }
+ 
+ void show_leds()
+ {
+	 // Buffering outputs
+	 if (APace == 1)
+	 {
+		 fprintf(fp, "A");
+		 LEDAPace = APace;
+		 LEDBufferCountA = LED_BUFFER;
+	 }
+ 
+	 if (VPace == 1)
+	 {
+		 fprintf(fp, "V");
+		 LEDVPace = VPace;
+		 LEDBufferCountV = LED_BUFFER;
+	 }
+ 
+	 LEDBufferCountA--;
+	 LEDBufferCountV--;
+ 
+	 if (LEDBufferCountA == 0) LEDAPace = 0;
+	 if (LEDBufferCountV == 0) LEDVPace = 0;
+ 
+	 // Outputting buffered values to LEDs
+	 if (LEDVPace == 1)
+	 {
+		 if (LEDAPace == 1) IOWR_ALTERA_AVALON_PIO_DATA(LEDS_GREEN_BASE, 0x03);
+		 else IOWR_ALTERA_AVALON_PIO_DATA(LEDS_GREEN_BASE, 0x01);
+	 }
+	 else
+	 {
+		 if (LEDAPace == 1) IOWR_ALTERA_AVALON_PIO_DATA(LEDS_GREEN_BASE, 0x02);
+		 else IOWR_ALTERA_AVALON_PIO_DATA(LEDS_GREEN_BASE, 0x00);
+	 }
+ }
+ 
  int main()
  {
 	 init_buttons_pio();
 	 up = open(UART_NAME, O_RDWR | O_NONBLOCK);
+	 fp = fopen(UART_NAME, "w+");
 	 reset();
+	 VSense = 1;
+	 LEDBufferCountA = 0;
+	 LEDBufferCountV = 0;
  
 	 while(1)
 	 {
-		 if ((nbr = read(up, buffer, 1)) > 0) {
-			 if (buffer[0] == 65) ASense = 1; // A
-			 if (buffer[0] == 86) VSense = 1; // V
-		 }
- 
+		 read_uart();
 		 tick();
- 
-		 // Starts the timers if event occurred
-		 if (AVI_start == 1) alt_alarm_start(&timer_AVI, AVI_VALUE, avi_timer_isr, NULL);
-		 if (AEI_start == 1) alt_alarm_start(&timer_AEI, AEI_VALUE, aei_timer_isr, NULL);
-		 if (PVARP_start == 1) alt_alarm_start(&timer_PVARP, PVARP_VALUE, pvarp_timer_isr, NULL);
-		 if (VRP_start == 1) alt_alarm_start(&timer_VRP, VRP_VALUE, vrp_timer_isr, NULL);
-		 if (LRI_start == 1) alt_alarm_start(&timer_LRI, LRI_VALUE, lri_timer_isr, NULL);
-		 if (URI_start == 1) alt_alarm_start(&timer_URI, URI_VALUE, uri_timer_isr, NULL);
- 
-		 // Stops the timers if event occurred
-		 if (AVI_stop == 1) alt_alarm_stop(&timer_AVI);
-		 if (AEI_stop == 1) alt_alarm_stop(&timer_AEI);
-		 if (PVARP_stop == 1) alt_alarm_stop(&timer_PVARP);
-		 if (VRP_stop == 1) alt_alarm_stop(&timer_VRP);
-		 if (LRI_stop == 1) alt_alarm_stop(&timer_LRI);
-		 if (URI_stop == 1) alt_alarm_stop(&timer_URI);
- 
-		 // reseting inputs
-		 VSense = 0;
-		 ASense = 0;
- 
-		 // resetting outputs
-		 AVITO = 0;
-		 PVARPTO = 0;
-		 VRPTO = 0;
-		 AEITO = 0;
-		 LRITO = 0;
-		 URITO = 0;
- 
-		 // Buffering outputs
-		 if (APace == 1)
-		 {
-			 LEDAPace = APace;
-			 LEDBufferCountA = LED_BUFFER;
-		 }
- 
-		 if (VPace == 1)
-		 {
-			 LEDVPace = VPace;
-			 LEDBufferCountV = LED_BUFFER;
-		 }
- 
-		 LEDBufferCountA--;
-		 LEDBufferCountV--;
- 
-		 if (LEDBufferCountA == 0) LEDAPace = 0;
-		 if (LEDBufferCountV == 0) LEDVPace = 0;
- 
-		 // Outputting buffered values to LEDs
-		 if (LEDVPace == 1)
-		 {
-			 if (LEDAPace == 1) IOWR_ALTERA_AVALON_PIO_DATA(LEDS_GREEN_BASE, 0x03);
-			 else IOWR_ALTERA_AVALON_PIO_DATA(LEDS_GREEN_BASE, 0x01);
-		 }
-		 else
-		 {
-			 if (LEDAPace == 1) IOWR_ALTERA_AVALON_PIO_DATA(LEDS_GREEN_BASE, 0x02);
-			 else IOWR_ALTERA_AVALON_PIO_DATA(LEDS_GREEN_BASE, 0x00);
-		 }
- 
+		 start_stop_timers();
+		 reset_inputs();
+		 show_leds();
 	 }
  
 	 close(up);
+	 fclose(fp);
 	 return 0;
  }
  
